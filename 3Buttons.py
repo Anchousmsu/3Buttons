ff35ef25dfea0b2349ade8894fd8c9a0257fabcc
Chocolate queen <NUT1997020501@yandex.ru>
2019-03-14 01:07:04 +0300

    Add button and close action

--- 3Buttons.py
+++ 3Buttons.py
@@ -1,4 +1,12 @@
 import tkinter as tk
 
 root = tk.Tk()
+b = tk.Button(root, text="Click me")
+
+def close_window(event):
+	root.destroy()
+
+b.bind('<Button-1>', close_window)
+
+b.pack(side = 'right')
 root.mainloop()
